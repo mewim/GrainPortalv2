3a839a71740471f23fd06d3c82422f290746f46c
mewim <liuc223@gmail.com>
2016-08-16 11:01:59 -0700

    Update index.js

--- index.js
+++ index.js
@@ -24,27 +24,27 @@ var api = new ParseServer({
 });
 
 // Parse Dashboard
-var dashboard = new ParseDashboard({
-    apps: [
-        {
-            serverURL: "http://grainportal.herokuapp.com/parse",
-            appId: "Hbzk6oLuByPRXDryfHxuNa9dWbikJJLi",
-            masterKey: "sK9hBhqvxBh4KbqjroLodVeTCKtFXcpP",
-            javascriptKey: "NOT USED",
-            restKey: "NOT USED",
-            appName: "grainportal",
-            production: true
-        }
-    ],
-    users: [
-        {
-            user: 'admin',
-            pass: 'password'
-        }
-    ]
-});
+// var dashboard = new ParseDashboard({
+//     apps: [
+//         {
+//             serverURL: "http://grainportal.herokuapp.com/parse",
+//             appId: "Hbzk6oLuByPRXDryfHxuNa9dWbikJJLi",
+//             masterKey: "sK9hBhqvxBh4KbqjroLodVeTCKtFXcpP",
+//             javascriptKey: "NOT USED",
+//             restKey: "NOT USED",
+//             appName: "grainportal",
+//             production: true
+//         }
+//     ],
+//     users: [
+//         {
+//             user: 'admin',
+//             pass: 'password'
+//         }
+//     ]
+// });
 
-app.use('/dashboard', dashboard);
+// app.use('/dashboard', dashboard);
 // Client-keys like the javascript key or the .NET key are not necessary with parse-server
 // If you wish you require them, you can set them as options in the initialization above:
 // javascriptKey, restAPIKey, dotNetKey, clientKey
