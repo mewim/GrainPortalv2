612cad25eee938e6ac369a16845326917efd3029
mewim <liuc223@gmail.com>
2016-08-17 22:13:46 -0700

    Upload Jekyll code.

---
+++ jekyll/assets/js/parse_init.js
@@ -0,0 +1,4 @@
+// Initialize Parse API 
+Parse.initialize("Hbzk6oLuByPRXDryfHxuNa9dWbikJJLi",
+                   "iffERp8rzgHwekt7Qy4eBkc4O59XcRe3");
+Parse.serverURL = "http://grainportal.herokuapp.com/parse";
